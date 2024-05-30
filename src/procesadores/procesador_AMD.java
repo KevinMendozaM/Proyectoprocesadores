b87ebf29c836a5495c6a4a1242fd7eba596dbde9
safronjos-admin <josue.guerra@epn.edu.ec>
2024-05-30 09:06:37 -0500

    ArielPrueba2

--- src/procesadores/procesador_AMD.java
+++ src/procesadores/procesador_AMD.java
@@ -1,4 +1,24 @@
 package procesadores;
 
-public class procesador_AMD {
+public class procesador_AMD extends procesador{
+
+    String sensor_temperatura;
+    public procesador_AMD() {
+
+    }
+    public procesador_AMD(String sensor_temperatura) {
+        this.sensor_temperatura = sensor_temperatura;
+    }
+
+    public String getSensor_temperatura() {
+        return sensor_temperatura;
+    }
+
+    public void setSensor_temperatura(String sensor_temperatura) {
+        this.sensor_temperatura = sensor_temperatura;
+    }
+    //imprimir datos
+    public void imprimir_datos_escritorio(){
+        System.out.println(sensor_temperatura);
+    }
 }
