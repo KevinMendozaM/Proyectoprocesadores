86eb1931a3bfd7bdf0d5e2b4768e1ef17593f48f
safronjos-admin <josue.guerra@epn.edu.ec>
2024-05-30 09:03:43 -0500

    ArielPrueba1

--- src/procesadores/procesador_intel.java
+++ src/procesadores/procesador_intel.java
@@ -2,5 +2,23 @@ package procesadores;
 
 public class procesador_intel extends procesador{
 
+    String tarjeta_video;
+    public procesador_intel() {
 
+    }
+    public procesador_intel(String tarjeta_video) {
+        this.tarjeta_video = tarjeta_video;
+    }
+
+    public String getTarjeta_video() {
+        return tarjeta_video;
+    }
+
+    public void setTarjeta_video(String tarjeta_video) {
+        this.tarjeta_video = tarjeta_video;
+    }
+    //imprimir datos
+    public void imprimir_datos_escritorio(){
+        System.out.println(tarjeta_video);
+    }
 }
